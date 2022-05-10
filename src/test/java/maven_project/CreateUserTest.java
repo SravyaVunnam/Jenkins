8b082418644c89307f766e78052a158c0ddf5617
SravyaVunnam <sravya3896@gmail.com>
2022-05-10 15:20:54 +0530

    Jenkins added

---
+++ src/test/java/maven_project/CreateUserTest.java
@@ -0,0 +1,14 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class CreateUserTest {
+	
+	
+	@Test
+	public void createUserTest()
+	{
+		System.out.println("Creating a user");
+	}
+
+}
---
+++ src/test/java/maven_project/ReadingDataFromCommandLineTest.java
@@ -0,0 +1,24 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class ReadingDataFromCommandLineTest {
+	
+	
+	@Test(groups = "smoke")
+	public void readingDataFromCommandLineTest()
+	{
+		String URL = System.getProperty("url");
+		String UN = System.getProperty("username");
+		String PWD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(UN);
+		System.out.println(PWD);
+	}
+	
+	@Test(groups = "regression")
+	public void regressionTest() {
+		System.out.println("regression testing");
+	}
+
+}
